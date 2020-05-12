8ddb8cb867ad21af2ac71c067380affe6c421f98
sudhanshu uniyal <uniyal1001@gmail.com>
2020-05-12 23:37:56 +0530

    bug fixed

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -6,14 +6,14 @@ function CreatePost() {
     <Page title="Create New Post">
       <form>
         <div className="form-group">
-          <label for="post-title" className="text-muted mb-1">
+          <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input autofocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autocomplete="off" />
+          <input autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
         </div>
 
         <div className="form-group">
-          <label for="post-body" className="text-muted mb-1 d-block">
+          <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
           <textarea name="body" id="post-body" className="body-content tall-textarea form-control" type="text"></textarea>
--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -25,19 +25,19 @@ function HomeGuest() {
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <label for="username-register" className="text-muted mb-1">
+              <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
               <input onChange={e => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
             </div>
             <div className="form-group">
-              <label for="email-register" className="text-muted mb-1">
+              <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
               <input onChange={e => setEmail(e.target.value)} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
             </div>
             <div className="form-group">
-              <label for="password-register" className="text-muted mb-1">
+              <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
               <input onChange={e => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
