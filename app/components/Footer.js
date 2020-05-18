fbc603a8eb9110e21c722254bb026cd8c5144295
sudhanshu uniyal <uniyal1001@gmail.com>
2020-05-18 16:48:01 +0530

    added original title for website

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
       <p className="m-0">
         Copyright &copy; 2020{" "}
         <a href="/" className="text-muted">
-          ComplexApp
+          Dibeto
         </a>
         . All rights reserved.
       </p>
